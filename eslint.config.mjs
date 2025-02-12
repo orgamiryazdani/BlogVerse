eda82dd9571bdd7dcbb6b68b562e98b05440fea0
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-12 10:05:58 +0330

    feat: Install prettier and config eslint and fix lint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -11,12 +12,15 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  prettierConfig,
   {
     settings: {
       react: { version: "detect" }
     },
+    plugins: { prettier: prettierPlugin },
     rules: {
-      "react/react-in-jsx-scope": "off"
+      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": "error"
     }
   }
-]
+];
