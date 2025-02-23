4e336ccc9315738e347be9c607fe6676011287e4
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-23 10:39:14 +0330

    feat: Import useGetProducts and handle loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-import { readData } from '@/core/http-service'
-import { Products } from '@/types/products.interface'
+'use client'
 import Image from 'next/image'
+import { useGetProducts } from '../hooks/use-products'
+import { ProductsPlaceholder } from './_components/placeholders/products'
 
-export default async function Home() {
-    const products: Products = await readData('/products?limit=12&skip=50')
+export default function Home() {
+    const { data, isLoading } = useGetProducts()
+
+    if (isLoading) return <ProductsPlaceholder />
 
     return (
         <div className="md:w-[70%] w-full px-3 md:px-0 mt-5 flex flex-wrap justify-between gap-5">
-            {products?.products.map(({ id, title, thumbnail, description }) => (
-                <div key={id} className="md:w-[385px] w-full h-fit bg-gray-700/40 rounded-md">
+            {data?.products.map(({ id, title, thumbnail, description }) => (
+                <div key={id} className="md:w-[385px] w-full h-fit cursor-pointer bg-gray-700/40 rounded-md">
                     <div className="w-full h-auto flex items-center justify-center">
                         <Image width={200} height={200} src={thumbnail} alt={title} loading="lazy" />
                     </div>
