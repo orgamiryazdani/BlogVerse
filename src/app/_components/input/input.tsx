578dcd54ad048e73b9a5cd12b55e9c1d8aea57b8
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-24 19:04:55 +0330

    feat: Change props and type

--- src/app/_components/input/input.tsx
+++ src/app/_components/input/input.tsx
@@ -6,8 +6,7 @@ export const Input = <TFormValues extends FieldValues>({
     register,
     errors,
     name,
-    placeholder,
-    autoFocus,
+    ...rest
 }: InputProps<TFormValues>) => {
     const error = get(errors, name)
     const hasError = !!error
@@ -15,10 +14,9 @@ export const Input = <TFormValues extends FieldValues>({
         <div className="flex flex-col w-full">
             <label className="mb-1">{label}</label>
             <input
-                autoFocus={autoFocus}
-                {...register(name)}
+                {...(register && name ? register(name) : {})}
                 className="focus:ring-2 ring-emerald-500 rounded-sm p-2 text-black"
-                placeholder={placeholder}
+                {...rest}
             />
             {hasError && <p className="text-red-500 text-sm mt-1">{error.message}</p>}
         </div>
--- src/app/_components/input/input.types.ts
+++ src/app/_components/input/input.types.ts
@@ -1,10 +1,9 @@
+import { InputHTMLAttributes } from 'react'
 import { DeepMap, FieldError, FieldValues, Path, UseFormRegister } from 'react-hook-form'
 
-export type InputProps<T extends FieldValues> = {
-    label: string
-    register: UseFormRegister<T>
+export type InputProps<T extends FieldValues> = InputHTMLAttributes<HTMLInputElement> & {
+    label?: string
+    register?: UseFormRegister<T>
     errors?: Partial<DeepMap<T, FieldError>>
-    name: Path<T>
-    placeholder?: string
-    autoFocus?: boolean
+    name?: Path<T>
 }
