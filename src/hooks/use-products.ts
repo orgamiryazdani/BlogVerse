154a9756104c9ec4b13b85273947c7d69d6ffc47
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-24 19:01:07 +0330

    feat: Add search key and get search value as props

--- src/hooks/use-products.ts
+++ src/hooks/use-products.ts
@@ -2,10 +2,10 @@ import { getProductsApi } from '@/services/products-service'
 import { Products } from '@/types/products.interface'
 import { useQuery, UseQueryResult } from '@tanstack/react-query'
 
-export const useGetProducts = () => {
+export const useGetProducts = ({ search }: { search: string }) => {
     const queryResult: UseQueryResult<Products> = useQuery({
-        queryKey: ['products'],
-        queryFn: () => getProductsApi(),
+        queryKey: ['products', search],
+        queryFn: () => getProductsApi({ search }),
     })
     const { data, isLoading, refetch } = queryResult
     return { data, isLoading, refetch }
