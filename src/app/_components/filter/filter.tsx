cadb2dbce70838961dc1c19b3ce1637771ee3258
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-24 19:21:08 +0330

    feat: Import search component

---
+++ src/app/_components/filter/filter.tsx
@@ -0,0 +1,9 @@
+import Search from './serach'
+
+export const Filter: React.FC = () => {
+    return (
+        <div className="w-[30%] h-[490px] rounded-md bg-gray-700/50 mt-5 px-[7px] pt-[5px] sticky top-4">
+            <Search />
+        </div>
+    )
+}
