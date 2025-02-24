94801d905c878169ac086798c058bb733c5b2739
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-24 19:45:10 +0330

    fix: Add suspense and fix useSearchParams build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+'use client'
+import { Suspense } from 'react'
 import { Filter } from './_components/filter'
 import { Products } from './_components/products'
 
 export default function Home() {
     return (
         <div className="flex gap-x-12">
-            <Products />
-            <Filter />
+            <Suspense fallback="loading...">
+                <Products />
+                <Filter />
+            </Suspense>
         </div>
     )
 }
