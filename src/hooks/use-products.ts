a62668096b0b12d409ab31d2103ac078898caf2a
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-25 19:11:14 +0330

    feat: Get skip query and set key

--- src/hooks/use-products.ts
+++ src/hooks/use-products.ts
@@ -2,10 +2,10 @@ import { getProductsApi } from '@/services/products-service'
 import { Products } from '@/types/products.interface'
 import { useQuery, UseQueryResult } from '@tanstack/react-query'
 
-export const useGetProducts = ({ search }: { search: string }) => {
+export const useGetProducts = ({ search, skip }: { search: string; skip: number }) => {
     const queryResult: UseQueryResult<Products> = useQuery({
-        queryKey: ['products', search],
-        queryFn: () => getProductsApi({ search }),
+        queryKey: ['products', search, skip],
+        queryFn: () => getProductsApi({ search, skip }),
     })
     const { data, isLoading, refetch } = queryResult
     return { data, isLoading, refetch }
