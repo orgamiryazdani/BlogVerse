a4040d127229fc216ac97777ee58e0c43f388893
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-25 19:09:55 +0330

    feat: Add limit and skip query

--- src/services/products-service.ts
+++ src/services/products-service.ts
@@ -1,6 +1,6 @@
 import { readData } from '@/core/http-service'
 import { Products } from '@/types/products.interface'
 
-export const getProductsApi = async ({ search }: { search: string }) => {
-    return await readData<Products>(`/products/search?q=${search}`)
+export const getProductsApi = async ({ search, skip }: { search: string; skip: number }) => {
+    return await readData<Products>(`/products/search?q=${search}&limit=10&skip=${skip}`)
 }
