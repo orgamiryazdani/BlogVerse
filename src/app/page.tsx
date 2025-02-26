3d78fcf5cfec74ccd524a6f851741d9831392f5d
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-26 11:07:07 +0330

    feat: Create loading and use for fallback in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@
 import { Suspense } from 'react'
 import { Filter } from './_components/filter'
 import { Products } from './_components/products'
+import { Loading } from './_components/loading'
 
 export default function Home() {
     return (
         <div className="flex lg:gap-x-12 gap-x-5">
-            <Suspense fallback="loading...">
+            <Suspense fallback={<Loading />}>
                 <Products />
                 <Filter />
             </Suspense>
