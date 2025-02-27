68fb1af96e81046c5b663c248b886958f415b833
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-27 23:32:31 +0330

    feat: Get the sortBy and order as props and add as key

--- src/hooks/use-products.ts
+++ src/hooks/use-products.ts
@@ -2,10 +2,20 @@ import { getProductsApi } from '@/services/products-service'
 import { Products } from '@/types/products.interface'
 import { useQuery, UseQueryResult } from '@tanstack/react-query'
 
-export const useGetProducts = ({ search, skip }: { search: string; skip: number }) => {
+export const useGetProducts = ({
+    search,
+    skip,
+    sortBy,
+    order,
+}: {
+    search: string
+    skip: number
+    sortBy: string
+    order: string
+}) => {
     const queryResult: UseQueryResult<Products> = useQuery({
-        queryKey: ['products', search, skip],
-        queryFn: () => getProductsApi({ search, skip }),
+        queryKey: ['products', search, skip, sortBy, order],
+        queryFn: () => getProductsApi({ search, skip, sortBy, order }),
     })
     const { data, isLoading, refetch } = queryResult
     return { data, isLoading, refetch }
