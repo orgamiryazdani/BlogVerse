718c8b9d0af1cebe6514eab1cff8b298fbe80a11
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-27 23:31:14 +0330

    feat: Add sortBy and order as a query

--- src/services/products-service.ts
+++ src/services/products-service.ts
@@ -1,6 +1,18 @@
 import { readData } from '@/core/http-service'
 import { Products } from '@/types/products.interface'
 
-export const getProductsApi = async ({ search, skip }: { search: string; skip: number }) => {
-    return await readData<Products>(`/products/search?q=${search}&limit=10&skip=${skip}`)
+export const getProductsApi = async ({
+    search,
+    skip,
+    sortBy,
+    order,
+}: {
+    search: string
+    skip: number
+    sortBy: string
+    order: string
+}) => {
+    return await readData<Products>(
+        `/products/search?q=${search}&limit=10&skip=${skip}&sortBy=${sortBy}&order=${order}`
+    )
 }
