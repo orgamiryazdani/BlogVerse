4cb86ac069c9b93a4a4958430d504b5299f1b819
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-27 21:47:19 +0330

    style: Change filter pt in mobile

--- src/app/_components/filter/filter.tsx
+++ src/app/_components/filter/filter.tsx
@@ -19,7 +19,7 @@ export const Filter: React.FC = () => {
                 ></div>
             )}
             <div
-                className={`md:w-[30%] w-full md:h-[490px] ${showFilter ? 'h-[75%]' : 'h-0'} transition-all duration-300 ease-in-out fixed left-0 right-0 bottom-0 md:rounded-md rounded-t-lg md:bg-gray-700/50 bg-gray-700 md:mt-5 px-[7px] md:pt-[5px] md:sticky md:top-4`}
+                className={`md:w-[30%] w-full md:h-[490px] ${showFilter ? 'h-[75%] pt-[5px]' : 'h-0'} transition-all duration-300 ease-in-out fixed left-0 right-0 bottom-0 md:rounded-md rounded-t-lg md:bg-gray-700/50 bg-gray-700 md:mt-5 px-[7px] md:pt-[5px] md:sticky md:top-4`}
             >
                 <Search />
             </div>
