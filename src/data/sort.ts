ea62e408574635a0e1d46e6d0666925bd0c97764
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-28 23:21:28 +0330

    feat: Add sort data

---
+++ src/data/sort.ts
@@ -0,0 +1,26 @@
+export const sortOptions = [
+    { value: 'id', label: 'ID' },
+    { value: 'title', label: 'Title' },
+    { value: 'description', label: 'Description' },
+    { value: 'category', label: 'Category' },
+    { value: 'price', label: 'Price' },
+    { value: 'discountPercentage', label: 'Discount Percentage' },
+    { value: 'rating', label: 'Rating' },
+    { value: 'stock', label: 'Stock' },
+    { value: 'tags', label: 'Tags' },
+    { value: 'brand', label: 'Brand' },
+    { value: 'sku', label: 'Product Code (SKU)' },
+    { value: 'weight', label: 'Weight' },
+    { value: 'dimensions', label: 'Dimensions' },
+    { value: 'warrantyInformation', label: 'Warranty Information' },
+    { value: 'shippingInformation', label: 'Shipping Information' },
+    { value: 'availabilityStatus', label: 'Availability Status' },
+    { value: 'reviews', label: 'Reviews' },
+    { value: 'returnPolicy', label: 'Return Policy' },
+    { value: 'minimumOrderQuantity', label: 'Minimum Order Quantity' },
+]
+
+export const orderOptions = [
+    { value: 'asc', label: 'ascending' },
+    { value: 'desc', label: 'Descending' },
+]
