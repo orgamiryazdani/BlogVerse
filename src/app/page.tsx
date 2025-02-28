221ff346e95d17cf74618689692f0c598bc66b14
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-28 23:22:07 +0330

    refactor: Change filter component name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 'use client'
 import { Suspense } from 'react'
-import { Filter } from './_components/filter-bar'
 import { Products } from './_components/products'
 import { Loading } from './_components/loading'
+import { FilterBar } from './_components/filter-bar'
 
 export default function Home() {
     return (
         <div className="flex lg:gap-x-12 gap-x-5">
             <Suspense fallback={<Loading />}>
                 <Products />
-                <Filter />
+                <FilterBar />
             </Suspense>
         </div>
     )
