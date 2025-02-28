e46801e341fc22c74ee9f03412946cce991cf688
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-28 23:28:04 +0330

    feat: Import remove filter

--- src/app/_components/filter-bar/sort.tsx
+++ src/app/_components/filter-bar/sort.tsx
@@ -1,31 +1,6 @@
 import useQueryParam from '@/hooks/use-query-param'
-
-const sortOptions = [
-    { value: 'id', label: 'ID' },
-    { value: 'title', label: 'Title' },
-    { value: 'description', label: 'Description' },
-    { value: 'category', label: 'Category' },
-    { value: 'price', label: 'Price' },
-    { value: 'discountPercentage', label: 'Discount Percentage' },
-    { value: 'rating', label: 'Rating' },
-    { value: 'stock', label: 'Stock' },
-    { value: 'tags', label: 'Tags' },
-    { value: 'brand', label: 'Brand' },
-    { value: 'sku', label: 'Product Code (SKU)' },
-    { value: 'weight', label: 'Weight' },
-    { value: 'dimensions', label: 'Dimensions' },
-    { value: 'warrantyInformation', label: 'Warranty Information' },
-    { value: 'shippingInformation', label: 'Shipping Information' },
-    { value: 'availabilityStatus', label: 'Availability Status' },
-    { value: 'reviews', label: 'Reviews' },
-    { value: 'returnPolicy', label: 'Return Policy' },
-    { value: 'minimumOrderQuantity', label: 'Minimum Order Quantity' },
-]
-
-const orderOptions = [
-    { value: 'asc', label: 'ascending' },
-    { value: 'desc', label: 'Descending' },
-]
+import { RemoveFilter } from './clear-filter-sort'
+import { orderOptions, sortOptions } from '@/data/sort'
 
 export const Sort: React.FC = () => {
     const { addQueryParam, searchParams } = useQueryParam()
@@ -38,16 +13,21 @@ export const Sort: React.FC = () => {
 
     return (
         <section className="flex flex-col">
-            <span className="mt-3 text-2xl font-bold w-full text-center">sort products</span>
-            <span className="mb-1 text-xl font-bold">sort by</span>
-            <div className="w-full h-auto max-h-36 overflow-y-auto flex flex-wrap gap-3">
+            <span className="mt-2 text-2xl font-bold w-full text-center text-emerald-500">sort products</span>
+            <span className="text-xl font-bold">sort by</span>
+            <div className="w-full h-auto max-h-28 overflow-y-auto flex flex-wrap gap-2 pt-1">
                 {sortOptions.map(({ value, label }) => (
                     <div
                         key={value}
                         onClick={() => sortHandler('sortBy', value)}
-                        className={`px-5 py-3 rounded-md md:bg-gray-700 bg-gray-600 cursor-pointer ${sortSelected === value ? 'border border-emerald-500' : ''}`}
+                        className={`px-4 py-3 rounded-md relative md:bg-gray-700 bg-gray-600 cursor-pointer ${sortSelected === value ? 'border border-emerald-500' : ''}`}
                     >
                         {label}
+                        <RemoveFilter
+                            selected={sortSelected}
+                            value={value}
+                            sortHandler={() => sortHandler('sortBy', '')}
+                        />
                     </div>
                 ))}
             </div>
@@ -57,9 +37,14 @@ export const Sort: React.FC = () => {
                     <div
                         key={value}
                         onClick={() => sortHandler('order', value)}
-                        className={`w-1/2 h-11 md:bg-gray-700 bg-gray-600 cursor-pointer rounded-md flex items-center justify-center text-lg font-bold ${orderSelected === value ? 'border border-emerald-500' : ''}`}
+                        className={`w-1/2 h-11 md:bg-gray-700 bg-gray-600 relative cursor-pointer rounded-md flex items-center justify-center text-lg font-bold ${orderSelected === value ? 'border border-emerald-500' : ''}`}
                     >
                         {label}
+                        <RemoveFilter
+                            selected={orderSelected}
+                            value={value}
+                            sortHandler={() => sortHandler('order', '')}
+                        />
                     </div>
                 ))}
             </div>
