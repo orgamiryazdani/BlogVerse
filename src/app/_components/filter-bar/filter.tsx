b9ba6a43424c5e27a9116c4d3698b56d7cec730a
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-28 23:25:59 +0330

    feat: Create filter products by category component

---
+++ src/app/_components/filter-bar/filter.tsx
@@ -0,0 +1,40 @@
+import { useGetProductsCategoryList } from '@/hooks/use-products'
+import useQueryParam from '@/hooks/use-query-param'
+import { CategoryPlaceholder } from '../placeholders/category'
+import { RemoveFilter } from './clear-filter-sort'
+
+export const Filter: React.FC = () => {
+    const { data, isLoading } = useGetProductsCategoryList()
+    const { addQueryParam, searchParams } = useQueryParam()
+    const selectedValue = searchParams.get('category') || ''
+
+    const filterHandler = (value: string) => {
+        addQueryParam('category', value)
+    }
+
+    if (isLoading) return <CategoryPlaceholder />
+
+    return (
+        <section className="w-full mt-3">
+            <span className="w-full mb-1 flex justify-center text-2xl font-bold text-emerald-500">
+                Filter by category
+            </span>
+            <div className="w-full h-32 overflow-y-auto flex flex-wrap gap-2 pt-1">
+                {data?.map((item) => (
+                    <span
+                        key={item}
+                        onClick={() => filterHandler(item)}
+                        className={`md:bg-gray-700 bg-gray-600 px-4 py-3 relative rounded-md cursor-pointer ${selectedValue === item ? 'border border-emerald-500' : ''}`}
+                    >
+                        {item}
+                        <RemoveFilter
+                            selected={selectedValue}
+                            value={item}
+                            sortHandler={() => filterHandler('')}
+                        />
+                    </span>
+                ))}
+            </div>
+        </section>
+    )
+}
