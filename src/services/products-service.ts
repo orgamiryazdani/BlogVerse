e42fa5f6f4563b5bdc3a316b46d45d53190150d6
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-28 23:19:03 +0330

    feat: Add get products category and change get products api

--- src/services/products-service.ts
+++ src/services/products-service.ts
@@ -1,8 +1,13 @@
 import { readData } from '@/core/http-service'
 import { Products, ProductsQueryValue } from '@/types/products.interface'
 
-export const getProductsApi = async ({ search, skip, sortBy, order }: ProductsQueryValue) => {
-    return await readData<Products>(
-        `/products/search?q=${search}&limit=10&skip=${skip}&sortBy=${sortBy}&order=${order}`
-    )
+export const getProductsApi = async ({ search, skip, sortBy, order, category }: ProductsQueryValue) => {
+    const query = category
+        ? `/category/${category}/?limit=10&skip=${skip}&sortBy=${sortBy}&order=${order}`
+        : `/search?q=${search}&limit=10&skip=${skip}&sortBy=${sortBy}&order=${order}`
+    return await readData<Products>(`/products${query}`)
+}
+
+export const getProductsCategoryListApi = async () => {
+    return await readData<string[]>('/products/category-List')
 }
