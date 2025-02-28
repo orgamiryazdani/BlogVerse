3e463cc85d6aadbdf825692db7db7d6b53c288b6
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-28 23:21:08 +0330

    feat: Add get products category hook and category query props

--- src/hooks/use-products.ts
+++ src/hooks/use-products.ts
@@ -1,11 +1,20 @@
-import { getProductsApi } from '@/services/products-service'
+import { getProductsApi, getProductsCategoryListApi } from '@/services/products-service'
 import { Products, ProductsQueryValue } from '@/types/products.interface'
 import { useQuery, UseQueryResult } from '@tanstack/react-query'
 
-export const useGetProducts = ({ search, skip, sortBy, order }: ProductsQueryValue) => {
+export const useGetProducts = ({ search, skip, sortBy, order, category }: ProductsQueryValue) => {
     const queryResult: UseQueryResult<Products> = useQuery({
-        queryKey: ['products', search, skip, sortBy, order],
-        queryFn: () => getProductsApi({ search, skip, sortBy, order }),
+        queryKey: ['products', search, skip, sortBy, order, category],
+        queryFn: () => getProductsApi({ search, skip, sortBy, order, category }),
+    })
+    const { data, isLoading, refetch } = queryResult
+    return { data, isLoading, refetch }
+}
+
+export const useGetProductsCategoryList = () => {
+    const queryResult: UseQueryResult<string[]> = useQuery({
+        queryKey: ['products-category-list'],
+        queryFn: () => getProductsCategoryListApi(),
     })
     const { data, isLoading, refetch } = queryResult
     return { data, isLoading, refetch }
