38816ac155426318ec252e6314d60411ddda4785
orgamiryazdani <orgamiryazdani@gmail.com>
2025-03-01 23:02:43 +0330

    feat: Add async await

--- src/app/(products)/product/[id]/page.tsx
+++ src/app/(products)/product/[id]/page.tsx
@@ -1,5 +1,5 @@
-const Product = ({ params }: { params: { id: string } }) => {
-    const productId = parseInt(params.id) || 1
+const Product = async ({ params }: { params: { id: string } }) => {
+    const productId = parseInt(await params.id) || 1
 
     return <div>{productId}</div>
 }
