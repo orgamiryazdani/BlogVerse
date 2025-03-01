f3857e5609f0fa163062426ea64b649a6fbf4e63
orgamiryazdani <orgamiryazdani@gmail.com>
2025-03-01 23:05:11 +0330

    refactor: Remove params

--- src/app/(products)/product/[id]/page.tsx
+++ src/app/(products)/product/[id]/page.tsx
@@ -1,7 +1,5 @@
-const Product = async ({ params }: { params: { id: string } }) => {
-    const productId = parseInt(await params.id) || 1
-
-    return <div>{productId}</div>
+const Product = () => {
+    return <div>1</div>
 }
 
 export default Product
