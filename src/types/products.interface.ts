5c3d9f9c644a29adde919ba948753200730e1ab0
orgamiryazdani <orgamiryazdani@gmail.com>
2025-03-02 23:52:09 +0330

    feat: Add single product type

--- src/types/products.interface.ts
+++ src/types/products.interface.ts
@@ -27,3 +27,49 @@ export type ProductsQueryValue = {
     order: string
     category: string
 }
+
+interface Review {
+    rating: number
+    comment: string
+    date: string
+    reviewerName: string
+    reviewerEmail: string
+}
+
+interface Dimensions {
+    width: number
+    height: number
+    depth: number
+}
+
+interface Meta {
+    createdAt: string
+    updatedAt: string
+    barcode: string
+    qrCode: string
+}
+
+export interface Product {
+    id: number
+    title: string
+    description: string
+    category: string
+    price: number
+    discountPercentage: number
+    rating: number
+    stock: number
+    tags: string[]
+    brand: string
+    sku: string
+    weight: number
+    dimensions: Dimensions
+    warrantyInformation: string
+    shippingInformation: string
+    availabilityStatus: string
+    reviews: Review[]
+    returnPolicy: string
+    minimumOrderQuantity: number
+    meta: Meta
+    thumbnail: string
+    images: string[]
+}
